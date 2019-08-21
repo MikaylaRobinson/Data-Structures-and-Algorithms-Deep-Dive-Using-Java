cfe5dce28ee2693643a593f9f87435c3f74a45c0
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-08-20 22:12:37 -0400

    Implemenation example for selection sort

---
+++ Sort_Algorithms/SelectionSort/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // This is an implementation of selection sort.
+        // Selection sort is a quadratic time complexity algorithm. (Big
+        // O notation of O(n^2).
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            // Setting the initial largest value to be at index 0, since all positions are unknown
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastUnsortedIndex);
+        }
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
