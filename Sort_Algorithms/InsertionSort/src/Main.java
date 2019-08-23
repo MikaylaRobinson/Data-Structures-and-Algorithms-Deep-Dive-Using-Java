19a3d2090c1c6660def1f2323fc1b7c9ae0939fa
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-08-22 22:31:40 -0400

    Formatting

--- Sort_Algorithms/InsertionSort/src/Main.java
+++ Sort_Algorithms/InsertionSort/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         // This is an implementation of insertion sort. The unsorted and sorted portions are
         // partitioned off. This works by assessing each element in the unsorted portion and comparing
         // it each value in the sorted portion until the conditions are met for it to be in its
-        // ordered position. The element is then inserted into that position. 
+        // ordered position. The element is then inserted into that position.
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;
